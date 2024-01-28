7fad308d13e6f090a3f0130e16798a8e733aab52
Jossi <jossimunoz@gmail.com>
2024-01-28 17:13:14 -0500

    login and signup added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
@@ -10,16 +11,22 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    
     path(route='about', view=views.about, name='about'),
-
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
     # path for registration
 
+     path(route='registration', view=views.registration_request, name='registration'),
+
     # path for login
 
+    path(route='login', view=views.login_request, name='login'),
+
+
     # path for logout
+
+    path(route='logout', view=views.logout_request, name='logout'),
+
 
     path(route='', view=views.get_dealerships, name='index'),
 
